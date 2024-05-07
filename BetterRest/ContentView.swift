f4cbfd5cd45268f4f7f2915087d4ca89c61e64e9
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-07 13:48:39 -0700

    Define sections for better UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -27,21 +27,27 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden() // voiceover will still read label
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden() // voiceover will still read label
-                
+                VStack(alignment: .leading, spacing: 0) {
                 Text("Desired amount of sleep")
                     .font(.headline)
                 
                 Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar {
